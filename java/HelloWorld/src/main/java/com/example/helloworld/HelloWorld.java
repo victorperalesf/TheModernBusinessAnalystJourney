6fa020445cf494359d084a2f938ca5827ca42956
Victor Perales <victor_peralesf@hotmail.com>
2017-07-20 18:09:58 +0100

    First Commit

---
+++ java/HelloWorld/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+/**
+ * Created by victorperales on 19/07/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args)  {
+
+        System.out.println("Hello champ!");
+
+    }
+}
